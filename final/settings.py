0e8f4bc4ae86eb326c0c8b1d3e9cee8979ebdc4e
gelargew <gelargew@gmail.com>
2021-01-12 19:44:04 +0700

    first commit

--- final/settings.py
+++ final/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import django_heroku
 from pathlib import Path
 import os
 
@@ -132,3 +132,4 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
 
 # Activate Django-Heroku.
+django_heroku.settings(locals())
